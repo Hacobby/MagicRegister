7843111bf20d15ae1c6e9d37f446ccf0d7ccdef4
Hacobby <elkinhacobby@gmail.com>
2025-05-12 16:46:05 -0500

    Actualizacion de estructuracion del proyecto

--- src/main/java/com/mycompany/magicregister/ProyectoPrestacion.java
+++ src/main/java/com/mycompany/magicregister/ProyectoPrestacion.java
@@ -2,8 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.proyectoprestacion;
-
+package com.mycompany.magicregister;
 import java.awt.Color;
 
 /**
---
+++ src/main/java/com/mycompany/magicregister/RegistroYControl.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.magicregister;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hacobby
+ */
+public class RegistroYControl {
+    
+    private ArrayList<Prestacion> equiposPrestados;
+    private ArrayList<Item> inventario;
+    
+    public RegistroYControl() {
+        inventario = new ArrayList();
+        equiposPrestados = new ArrayList();
+    }
+    
+    public void agregarItem(String item, int cantidad) {
+        inventario.add(new Item(item, cantidad));
+    }
+    
+    public void agregarPrestamo(String nombre, String ocupacion, int id, String horaEntrega, String horaRecibo, int salon, String item) {
+        equiposPrestados.add(new Prestacion(nombre, ocupacion, id, horaEntrega, horaRecibo, salon, item));
+    };
+    
+    // Set & Get
+    
+    public ArrayList<Prestacion> getEquiposPrestados() {
+        return this.equiposPrestados;
+    }
+    
+    public ArrayList<Item> getInventario() {
+        return this.inventario;
+    }
+}
